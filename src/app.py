0ad3d763fcf80cc357f3b364a305910d11f41cef
Priyanka Yadav <122746503+Priyankadsyadav@users.noreply.github.com>
2025-05-19 15:39:08 +0200

    Update

--- src/app.py
+++ src/app.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from src.logger import logging
+from src.exception import CustomException
+import os,sys
+
 
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    logging.info("I am testing here my second method of logging")
-    return "Welcome to Salary prediction by Priyanka Yadav"
+    try:
+        raise Exception("I am testing here my second method of logging")
+    except Exception as e:
+        abc=CustomException(e,sys)
+        logging.info(abc.error_message)
+        return "Welcome to Salary prediction by Priyanka Yadav"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
